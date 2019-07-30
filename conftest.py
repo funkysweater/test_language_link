40d8effdda9596d320404e6782f9994bc7e94500
mshpchb <krakentheseamonster@gmail.com>
2019-07-30 17:40:44 +0300

    removed browser-related stuff

--- conftest.py
+++ conftest.py
@@ -1,34 +1,21 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default="chrome",
-                     help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default='es',
+    parser.addoption('--language', action='store', default="fr",
                      help="Choose language: en-gb, ru, es, fr")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
     language = request.config.getoption("language")
+    browser = None
+    print("\nstarting test..")
+    from selenium.webdriver.chrome.options import Options
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': language})
     browser = webdriver.Chrome(options=options)
-    browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        fp = webdriver.FirefoxProfile()
-        fp.set_preference("intl.accept_languages", language)
-        browser = webdriver.Firefox(firefox_profile=fp)
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,10 +1,9 @@
-import pytest
+from selenium.webdriver.common.by import By
 import time
 
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_language_link(browser, language):
-    link = "http://selenium1py.pythonanywhere.com/{}/catalogue/coders-at-work_207/".format(language)
+def test_button_tobasket_is_avaliable(browser):
     browser.get(link)
-    # time.sleep(5)
     browser.find_element_by_css_selector(".btn-add-to-basket").click()
     # time.sleep(5)
